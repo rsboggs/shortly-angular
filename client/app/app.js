b5c61d9fc86864d6c28f4df50dd3cd6c8f169ae3
rsboggs <robertstephenboggs@gmail.com>
2016-02-12 16:12:58 -0800

    pass route tests

--- client/app/app.js
+++ client/app/app.js
@@ -14,8 +14,16 @@ angular.module('shortly', [
     .when('/signup', {
       templateUrl: 'app/auth/signup.html',
       controller: 'AuthController'
+    })
+    .when('/links', {
+      templateUrl: 'app/links/links.html',
+      controller: 'LinksController'
+    })
+    .when('/shorten', {
+      templateUrl: 'app/shorten/shorten.html',
+      controller: 'ShortenController'
     });
-    // Your code here
+    
 
     // We add our $httpInterceptor into the array
     // of interceptors. Think of it like middleware for your ajax calls
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,7 +2,7 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   $scope.link = {};
-  $scope.addLink = function(url) {
+  $scope.addLink = function (url) {
     Links.addOne(url);
   };
 });
